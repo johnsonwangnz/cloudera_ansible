711748a64e8f2bb721ebb9d6d501b1d8febfe88e
johnsonwangnz <johnsonwangnz@gmail.com>
2018-01-17 15:09:58 +1300

    add hadoop client role

--- roles/cdh_hadoop_config/templates/hadoop-env.sh
+++ roles/cdh_hadoop_config/templates/hadoop-env.sh
@@ -2,6 +2,6 @@
 
 export HADOOP_HEAPSIZE={{ heapsize }}
 
-export HADOOP_OPTS="-XX:+UseConcMarkSweepGC -XX:+AggressiveOpts -XX:MaxPermSize=300M -XX:+CMSClassUnloadingEnabled -XX:SurvivorRatio=8 -XX:+ExplicitGCInvokesConcurrent -verbose:gc -XX:+PrintGCDetails -XX:+PrintGCTimeStamps -Xloggc:/var/log/hadoop-hdfs/hadoop-hdfs-gc.log"
+export HADOOP_OPTS="-XX:+UseConcMarkSweepGC -XX:+AggressiveOpts -XX:+CMSClassUnloadingEnabled -XX:SurvivorRatio=8 -XX:+ExplicitGCInvokesConcurrent -verbose:gc -XX:+PrintGCDetails -XX:+PrintGCTimeStamps -Xloggc:/var/log/hadoop-hdfs/hadoop-hdfs-gc.log"
 
 
